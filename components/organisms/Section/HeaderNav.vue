022d24691ca5a82e3c1829ac0743b3700e98c1bf
ysd-marrrr <41146529+ysd-marrrr@users.noreply.github.com>
2020-04-30 23:31:49 +0900

    ヘッダーにデータを入れられるようにした

--- components/organisms/Section/HeaderNav.vue
+++ components/organisms/Section/HeaderNav.vue
@@ -2,18 +2,33 @@
   <section id="header">
     <h1>みんなのポートフォリオ！</h1>
     <nav>
-      <p>最終更新日</p>
+      <p>
+        {{ portfolioCountProp }}件掲載中 / 最終更新日: {{ lastUpdatedProp }}
+      </p>
     </nav>
   </section>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-export default Vue.extend({})
+export default Vue.extend({
+  props: {
+    portfolioCountProp: {
+      type: Number,
+      default: 0
+    },
+    lastUpdatedProp: {
+      type: String,
+      default: '2020-04-30'
+    }
+  }
+})
 </script>
 
 <style lang="scss" scoped>
 #header {
   display: flex;
+  align-items: center;
+  justify-content: space-around;
 }
 </style>
