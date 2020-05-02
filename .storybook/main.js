dddfae85901c3a0652dde8bcc86b1997a845bdd6
ysd-marrrr <41146529+ysd-marrrr@users.noreply.github.com>
2020-05-02 23:56:17 +0900

    Storybookでtailwindcssを読み込むようにした

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,7 +6,7 @@ module.exports = {
   webpackFinal: async (config, { configType }) => {
     config.resolve.alias['@'] = path.resolve(__dirname, '../')
     config.module.rules.push({
-      test: /\.scss$/,
+      test: /\.(s*)css$/,
       use: [
         'style-loader',
           {
@@ -17,7 +17,17 @@ module.exports = {
             options: {
               sourceMap: true
             }
-          }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: [
+                require('tailwindcss'),
+                require('autoprefixer')
+              ],
+            },
+          },
         ]
     });
     return config;
