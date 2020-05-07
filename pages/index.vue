ab987c65dc6a5648fd96d3bf358eaab8379382fd
ysd-marrrr <41146529+ysd-marrrr@users.noreply.github.com>
2020-05-07 23:35:58 +0900

    indexページを作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,14 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        everyone-s-portfolio-it
-      </h1>
-      <h2 class="subtitle">
-        My super-excellent Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <top />
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import Logo from '~/components/Logo.vue'
+import Top from '@/components/templates/Top.vue'
 
 export default Vue.extend({
   components: {
-    Logo
+    Top
   }
 })
 </script>
