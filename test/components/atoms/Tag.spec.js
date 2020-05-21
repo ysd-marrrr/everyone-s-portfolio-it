761f8eb44f21c0ec52be3581091ee34e65fbb9f6
ysd-marrrr <41146529+ysd-marrrr@users.noreply.github.com>
2020-05-21 23:29:00 +0900

    Tagのテストを追加

---
+++ test/components/atoms/Tag.spec.js
@@ -0,0 +1,14 @@
+import { mount } from '@vue/test-utils'
+import Tag from '@/components/atoms/Tag/Tag.vue'
+
+describe('Tag', () => {
+  it('Tagのslotが正しくセットされているか確認', () => {
+    const sampleText = 'Vue.js'
+    const wrapper = mount(Tag, {
+      slots: {
+        default: sampleText
+      }
+    })
+    expect(wrapper.find('span.tag').text()).toBe(sampleText)
+  })
+})
