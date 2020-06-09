02544a03303078057022dcc0d32e7bda6a2ea6e3
ysd-marrrr <41146529+ysd-marrrr@users.noreply.github.com>
2020-06-09 23:37:14 +0900

    ポートフォリオのリストをページネーション付きで取得できるようにした

--- backend/src/app/Http/Controllers/PortfolioController.php
+++ backend/src/app/Http/Controllers/PortfolioController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class PortfolioController extends Controller
@@ -13,7 +14,8 @@ class PortfolioController extends Controller
      */
     public function index()
     {
-        //
+        $portfolios = DB::table('portfolios')->paginate(20);
+        return response()->json($portfolios, 200, [], JSON_UNESCAPED_UNICODE);
     }
 
     /**
