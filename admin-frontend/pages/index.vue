5d3bffebc96d400a223d794196bf0eb0ee80abba
ysd-marrrr <41146529+ysd-marrrr@users.noreply.github.com>
2020-07-20 00:01:08 +0900

    admin-frontend: トップページの仮作成

--- admin-frontend/pages/index.vue
+++ admin-frontend/pages/index.vue
@@ -1,9 +1,26 @@
 <template>
   <v-layout column justify-center align-center>
+    <v-container>
+      <v-row>
+        <v-col>
+          <v-card class="pa-2" outlined tile>
+            <h2>未処理の登録申請数</h2>
+            <p class="counter-number">0</p>
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card class="pa-2" outlined tile>
+            <h2>登録総数</h2>
+            <p class="counter-number">0</p>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
     <v-container fluid>
-      <v-card>
+      <v-card outlined tile>
         <h2>登録されているポートフォリオ一覧(簡易)</h2>
         <portfolio-table-view :portfolio-list-prop="apiResult.data" />
+        <p>さらに表示する場合はデータ管理を開いてください。</p>
       </v-card>
     </v-container>
   </v-layout>
@@ -24,3 +41,9 @@ export default Vue.extend({
   },
 })
 </script>
+
+<style lang="scss" scoped>
+.counter-number {
+  font-size: 300%;
+}
+</style>
